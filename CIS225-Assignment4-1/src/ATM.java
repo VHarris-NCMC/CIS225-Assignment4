f0d75cc3ef3205a1b89d916c8eebc2dcfe8d2282
VHarris-NCMC <harrisv@student.ncmich.edu>
2023-03-07 19:40:03 -0500

    First Commit
    Framework / State Machines implemented.  Just need to add logic and clean up UI

---
+++ CIS225-Assignment4-1/src/ATM.java
@@ -0,0 +1,17 @@
+import java.net.NetworkInterface;
+
+public class ATM {
+
+    private CardReader _reader;
+    private PinPad _pinPad;
+    private Dispay _display;
+    private NetworkInterface _networkInterface;
+
+
+
+
+
+
+
+
+}
---
+++ CIS225-Assignment4-1/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    private static ATM atm_instance;
+
+
+    public static void main(String[] args)
+    {
+
+    }
+}
